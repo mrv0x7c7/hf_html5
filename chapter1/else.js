469cf3112aef379e9b356cae31cb07b6ff246665
mrvmix <romanmishedchenko@gmail.com>
2021-09-17 07:13:20 +0300

    added else.js

---
+++ chapter1/else.js
@@ -0,0 +1,18 @@
+var scoops = 10;
+while (scoops >= 0) {
+    if (scoops == 3) {
+        alert ("Ice cream is running low!");
+    } else if (scoops >9) {
+        alert ("Eat faster, the ice cream is going to melt");
+    } else if (scoops == 2) {
+        alert ("Going once!");
+    } else if (scoops == 1) {
+        alert ("Going twice");
+    } else if (scoops == 0) {
+        alert ("Gone!");
+    } else {
+        alert("Still lots of ice cream left, come and get it.")
+    }
+    scoops = scoops - 1;
+}
+alert ("life  without ice cream isn't the same");
---
+++ chapter1/var count = 0.js
@@ -0,0 +1,11 @@
+var count = 0;
+for (var i=0; i<5; i++) {
+        count=count+i;
+}
+alert ("count is"+count)
+
+
+0+1=1
+1+2=3
+3+3=6
+6+4=10
